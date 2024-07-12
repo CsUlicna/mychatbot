dc9b15c1f9f001750edf559bdc28aebc5bd6a853
CsUlicna <ulicna@pulib.sk>
2024-07-12 11:07:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
